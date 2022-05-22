99a81769ebb039b3163dd2b7aab82d895b5ab411
Jesus <barthjoss@hotmail.es>
2022-05-22 16:58:25 -0500

    changes

--- first_project/frontend/src/App.js
+++ first_project/frontend/src/App.js
@@ -2,11 +2,8 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 flex justify-center">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <Form/>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border m-10 flex justify-center">
+      <Form />
     </div>
   );
 }
--- first_project/frontend/src/components/Form.jsx
+++ first_project/frontend/src/components/Form.jsx
@@ -1,15 +1,39 @@
 import React, { useState } from "react";
+import LogoSubmit from '../assets/submit.png';
+import axios from 'axios';
 
-export default function Form(){
-    const [name,setName] = useState('')
-    const [lastName,setlastName] = useState('')
+export default function Form() {
+    const [name, setName] = useState('')
+    const [lastName, setlastName] = useState('')
     const [salaray, setSalary] = useState(0)
 
-    return(
-        <div className="container">
-            <form className="">
+    const storage = async () => {
+        const NewEmployee = { name, lastName, salaray }
+        const response = await axios.post('')
+    }
 
-            </form>
-        </div>
+    return (
+        <div className="leading-loose">
+            <form className="max-w-4xl m-4 p-10 bg-white rounded shadow-xl place-content-center">
+                <p className="text-gray-800 font-medium">Customer information</p>
+                <div className="mt-2">
+                    <label className="block text-sm text-gray-00" >Name</label>
+                    <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="cus_name" name="cus_name" type="text" required="" placeholder="Your Name" aria-label="Name" onChange={e => setName(e.target.value)} />
+                </div>
+                <div className="mt-2">
+                    <label className="block text-sm text-gray-600" >Last name</label>
+                    <input className="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="Your last name" aria-label="Email" onChange={e => setlastName(e.target.value)} />
+                </div>
+                <div className="mt-2">
+                    <label className="block text-sm text-gray-600" >Your salary</label>
+                    <input className="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="Your salary" aria-label="Email" onChange={e => setSalary(e.target.value)} />
+                </div>
+                <div className="mt-4 flex place-content-center">
+                    <button type="submit" className="">
+                        <img src={LogoSubmit} alt="" />
+                    </button>
+                </div>
+            </form >
+        </div >
     )
 }
