3cb6f61be5871859c881170eaa7fda94b61949f1
Jesus Gonzalez <barthjoss@hotmail.es>
2022-05-22 16:05:14 -0500

    New component form

--- first_project/frontend/src/App.js
+++ first_project/frontend/src/App.js
@@ -1,12 +1,12 @@
+import Form from "./components/Form";
+
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 flex justify-center">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <Form/>
     </div>
   );
 }
---
+++ first_project/frontend/src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+export default function Form(){
+    const [name,setName] = useState('')
+    const [lastName,setlastName] = useState('')
+    const [salaray, setSalary] = useState(0)
+
+    return(
+        <div className="container">
+            <form className="">
+
+            </form>
+        </div>
+    )
+}
